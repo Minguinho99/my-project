d5c039b4f24dfd0db8d856f4b96dfb5ddfb6a6d4
Minguinho99 <jyjeon99@kaist.ac.kr>
2017-06-04 23:22:33 +0900

    add

--- poll/views.py
+++ poll/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
 from django.utils import timezone
 from .models import Post
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .forms import PostForm
+
+def home(request):
+    return render(request, 'poll/home.html', {})
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -20,7 +23,13 @@ def post_new(request):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail', pk=post.pk)
+            return redirect('poll.views.post_datail', pk=post.pk)
     else:
         form = PostForm()
-    return render(request, 'poll/post_edit.html', {'form': form})
+    return render(request, 'poll/post_substance.html', {'form': form})
+
+def longin(request):
+    return render(request, 'poll/login.html', {})
+
+def signup(request):
+    return render(request, 'poll/sign_up.html', {})
